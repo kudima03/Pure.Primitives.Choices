438ab1d8c1e5d94c3b776050cdb178dd632e6c38
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-16 15:54:15 +0300

    Implemented choice for bool primitive.

---
+++ src/Pure.Primitives/Bool/BoolChoice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pure.Primitives.Bool;
+
+public sealed record BoolChoice : IBool
+{
+    private readonly IBool _condition;
+
+    private readonly IBool _valueOnTrue;
+
+    private readonly IBool _valueOnFalse;
+
+    public BoolChoice(IBool condition, IBool valueOnTrue, IBool valueOnFalse)
+    {
+        _condition = condition;
+        _valueOnTrue = valueOnTrue;
+        _valueOnFalse = valueOnFalse;
+    }
+
+    bool IBool.Value => _condition.Value ? _valueOnTrue.Value : _valueOnFalse.Value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}

cde696345269f8058b298fb413252ac0b1c0008a
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-16 17:03:46 +0300

    Added tests for StringChoice.

---
+++ src/Tests/Pure.Primitives.Tests/String/StringChoiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Pure.Primitives.Bool;
+using Pure.Primitives.String;
+
+namespace Pure.Primitives.Tests.String;
+
+using String = Primitives.String.String;
+
+public sealed record StringChoiceTests
+{
+    [Fact]
+    public void CorrectChooseOnTrueCondition()
+    {
+        IString valueOnTrue = new String("1");
+        IString valueOnFalse = new String("0");
+        IString choice = new StringChoice(new True(), valueOnTrue, valueOnFalse);
+        Assert.Equal(valueOnTrue.Value, choice.Value);
+    }
+
+    [Fact]
+    public void CorrectChooseOnFalseCondition()
+    {
+        IString valueOnTrue = new String("1");
+        IString valueOnFalse = new String("0");
+        IString choice = new StringChoice(new False(), valueOnTrue, valueOnFalse);
+        Assert.Equal(valueOnFalse.Value, choice.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new StringChoice(new True(), new String("123"), new String("321")).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new StringChoice(new False(), new String("123"), new String("321")).ToString());
+    }
+}

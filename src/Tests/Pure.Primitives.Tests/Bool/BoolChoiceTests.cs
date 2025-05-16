b07714e949d9941772baae003eda3ccff0b27e46
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-16 16:07:41 +0300

    Added tests for BoolChoice.

---
+++ src/Tests/Pure.Primitives.Tests/Bool/BoolChoiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Pure.Primitives.Bool;
+
+namespace Pure.Primitives.Tests.Bool;
+
+public sealed record BoolChoiceTests
+{
+    [Fact]
+    public void CorrectChooseOnTrueCondition()
+    {
+        IBool choice = new BoolChoice(new True(), new True(), new False());
+        Assert.True(choice.Value);
+    }
+
+    [Fact]
+    public void CorrectChooseOnFalseCondition()
+    {
+        IBool choice = new BoolChoice(new False(), new True(), new False());
+        Assert.False(choice.Value);
+    }
+
+    [Fact]
+    public void ThrowExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new BoolChoice(new False(), new True(), new False()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new BoolChoice(new True(), new False(), new False()).ToString());
+    }
+}

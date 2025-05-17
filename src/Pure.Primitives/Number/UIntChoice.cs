bc7d8840f13de1932a1165e5ec2a2d19ab1d11da
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-17 14:14:29 +0300

    Implemented choice for uint primitive.

---
+++ src/Pure.Primitives/Number/UIntChoice.cs
@@ -0,0 +1,21 @@
+﻿using Pure.Primitives.Bool;
+
+namespace Pure.Primitives.Number;
+
+public sealed record UIntChoice : IUInt
+{
+    private readonly IBool _condition;
+
+    private readonly IUInt _valueOnTrue;
+
+    private readonly IUInt _valueOnFalse;
+
+    public UIntChoice(IBool condition, IUInt valueOnTrue, IUInt valueOnFalse)
+    {
+        _condition = condition;
+        _valueOnTrue = valueOnTrue;
+        _valueOnFalse = valueOnFalse;
+    }
+
+    uint IUInt.Value => _condition.Value ? _valueOnTrue.Value : _valueOnFalse.Value;
+}

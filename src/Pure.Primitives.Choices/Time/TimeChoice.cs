7cf77a6f4946c13250a431b0aa96c37b834cdc2e
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-28 11:22:26 +0300

    Implemented TimeChoice.

---
+++ src/Pure.Primitives.Choices/Time/TimeChoice.cs
@@ -0,0 +1,41 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.Number;
+using Pure.Primitives.Abstractions.Time;
+
+namespace Pure.Primitives.Choices.Time;
+
+public sealed record TimeChoice : ITime
+{
+    private readonly IBool _condition;
+
+    private readonly ITime _valueOnTrue;
+
+    private readonly ITime _valueOnFalse;
+
+    public TimeChoice(IBool condition, ITime valueOnTrue, ITime valueOnFalse)
+    {
+        _condition = condition;
+        _valueOnTrue = valueOnTrue;
+        _valueOnFalse = valueOnFalse;
+    }
+
+    public INumber<ushort> Hour => _condition.BoolValue ? _valueOnTrue.Hour : _valueOnFalse.Hour;
+
+    public INumber<ushort> Minute => _condition.BoolValue ? _valueOnTrue.Minute : _valueOnFalse.Minute;
+
+    public INumber<ushort> Second => _condition.BoolValue ? _valueOnTrue.Second : _valueOnFalse.Second;
+
+    public INumber<ushort> Millisecond => _condition.BoolValue ? _valueOnTrue.Millisecond : _valueOnFalse.Millisecond;
+
+    public INumber<ushort> Microsecond => _condition.BoolValue ? _valueOnTrue.Microsecond : _valueOnFalse.Microsecond;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}

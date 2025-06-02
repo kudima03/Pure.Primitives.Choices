c2043380e09ceb56f4579cb02e9653bc10a4a567
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-02 11:08:17 +0300

    Improved tests for DateChoice.

--- src/Tests/Pure.Primitives.Choices.Tests/Date/DateChoiceTests.cs
+++ src/Tests/Pure.Primitives.Choices.Tests/Date/DateChoiceTests.cs
@@ -1,6 +1,9 @@
-﻿using Pure.Primitives.Abstractions.Date;
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.Date;
 using Pure.Primitives.Bool;
 using Pure.Primitives.Choices.Date;
+using Randomizer;
+using Randomizer.Interfaces.ValueTypes;
 
 namespace Pure.Primitives.Choices.Tests.Date;
 
@@ -11,39 +14,72 @@ public sealed record DateChoiceTests
     [Fact]
     public void CorrectChooseOnTrueCondition()
     {
-        IDate valueOnTrue = new Date(new DateOnly(2024, 11, 5));
-        IDate valueOnFalse = new Date(new DateOnly(2025, 6, 1));
-        IDate choice = new DateChoice(new True(), valueOnTrue, valueOnFalse);
-        Assert.Equal(
-            new DateOnly(valueOnTrue.Year.NumberValue, valueOnTrue.Month.NumberValue, valueOnTrue.Day.NumberValue),
-            new DateOnly(choice.Year.NumberValue, choice.Month.NumberValue, choice.Day.NumberValue));
+        IRandomDateTime dateTimeGenerator = new RandomDateTimeGenerator();
+
+        IEnumerable<IDate> trueValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue())
+            .Select(x => new Date(DateOnly.FromDateTime(x)))
+            .ToArray();
+
+        IEnumerable<IDate> falseValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue())
+            .Select(x => new Date(DateOnly.FromDateTime(x)))
+            .ToArray();
+
+        IBool condition = new True();
+
+        IEnumerable<IDate> choices = trueValues.Zip(falseValues,
+            (trueValue, falseValue) => new DateChoice(condition, trueValue, falseValue));
+
+        Assert.Equal(trueValues, choices, (choice, dateTime) =>
+            choice.Year.NumberValue == dateTime.Year.NumberValue &&
+            choice.Month.NumberValue == dateTime.Month.NumberValue &&
+            choice.Day.NumberValue == dateTime.Day.NumberValue);
     }
 
     [Fact]
     public void CorrectChooseOnFalseCondition()
     {
-        IDate valueOnTrue = new Date(new DateOnly(2024, 11, 5));
-        IDate valueOnFalse = new Date(new DateOnly(2025, 6, 1));
-        IDate choice = new DateChoice(new False(), valueOnTrue, valueOnFalse);
-        Assert.Equal(
-            new DateOnly(valueOnFalse.Year.NumberValue, valueOnFalse.Month.NumberValue, valueOnFalse.Day.NumberValue),
-            new DateOnly(choice.Year.NumberValue, choice.Month.NumberValue, choice.Day.NumberValue));
+        IRandomDateTime dateTimeGenerator = new RandomDateTimeGenerator();
+
+        IEnumerable<IDate> trueValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue())
+            .Select(x => new Date(DateOnly.FromDateTime(x)))
+            .ToArray();
+
+        IEnumerable<IDate> falseValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue())
+            .Select(x => new Date(DateOnly.FromDateTime(x)))
+            .ToArray();
+
+        IBool condition = new False();
+
+        IEnumerable<IDate> choices = trueValues.Zip(falseValues,
+            (trueValue, falseValue) => new DateChoice(condition, trueValue, falseValue));
+
+        Assert.Equal(falseValues, choices, (choice, dateTime) =>
+            choice.Year.NumberValue == dateTime.Year.NumberValue &&
+            choice.Month.NumberValue == dateTime.Month.NumberValue &&
+            choice.Day.NumberValue == dateTime.Day.NumberValue);
     }
 
     [Fact]
     public void ThrowExceptionOnGetHashCode()
     {
         Assert.Throws<NotSupportedException>(() =>
-            new DateChoice(new True(), new Date(DateOnly.FromDateTime(System.DateTime.Now)),
-                new Date(DateOnly.FromDateTime(System.DateTime.Now))).GetHashCode());
+            new DateChoice(new True(),
+                new Date(DateOnly.MinValue),
+                new Date(DateOnly.MinValue))
+                .GetHashCode());
     }
 
     [Fact]
     public void ThrowExceptionOnToString()
     {
-        Assert.Throws<NotSupportedException>(() => new DateChoice(new False(),
-                new Date(DateOnly.FromDateTime(System.DateTime.Now)),
-                new Date(DateOnly.FromDateTime(System.DateTime.Now)))
-            .ToString());
+        Assert.Throws<NotSupportedException>(() =>
+            new DateChoice(
+                new False(), 
+                new Date(DateOnly.MinValue), 
+                new Date(DateOnly.MinValue)).ToString());
     }
 }

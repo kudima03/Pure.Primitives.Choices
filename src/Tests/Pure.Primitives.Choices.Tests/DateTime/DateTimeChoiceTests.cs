8f4d0f3c2ba7c58639efb869dbb5284785d4683a
Dmitry Kurochkin <kudima03@gmail.com>
2025-06-02 11:06:58 +0300

    Improved tests for DateTimeChoice.

--- src/Tests/Pure.Primitives.Choices.Tests/DateTime/DateTimeChoiceTests.cs
+++ src/Tests/Pure.Primitives.Choices.Tests/DateTime/DateTimeChoiceTests.cs
@@ -1,7 +1,9 @@
-﻿using Pure.Primitives.Abstractions.DateTime;
+﻿using Pure.Primitives.Abstractions.Bool;
+using Pure.Primitives.Abstractions.DateTime;
 using Pure.Primitives.Bool;
 using Pure.Primitives.Choices.DateTime;
-using Pure.Primitives.Choices.Time;
+using Randomizer;
+using Randomizer.Interfaces.ValueTypes;
 
 namespace Pure.Primitives.Choices.Tests.DateTime;
 
@@ -14,63 +16,71 @@ public sealed record DateTimeChoiceTests
     [Fact]
     public void CorrectChooseOnTrueCondition()
     {
-        System.DateTime dateTimeOnTrue = new System.DateTime(2024, 11, 5, 1, 2, 3, 4, 5).AddTicks(1);
-        System.DateTime dateTimeOnFalse = new System.DateTime(2025, 12, 13, 6, 5, 4, 3, 2).AddTicks(2);
-        IDateTime valueOnTrue = new DateTime(new Date(DateOnly.FromDateTime(dateTimeOnTrue)), new Time(TimeOnly.FromDateTime(dateTimeOnTrue)));
-        IDateTime valueOnFalse = new DateTime(new Date(DateOnly.FromDateTime(dateTimeOnFalse)), new Time(TimeOnly.FromDateTime(dateTimeOnFalse)));
-        IDateTime choice = new DateTimeChoice(new True(), valueOnTrue, valueOnFalse);
-        Assert.Equal(
-            new System.DateTime(
-                new DateOnly(valueOnTrue.Year.NumberValue,
-                    valueOnTrue.Month.NumberValue,
-                    valueOnTrue.Day.NumberValue),
-                new TimeOnly(valueOnTrue.Hour.NumberValue,
-                    valueOnTrue.Minute.NumberValue,
-                    valueOnTrue.Second.NumberValue,
-                    valueOnTrue.Millisecond.NumberValue,
-                    valueOnTrue.Microsecond.NumberValue)),
-            new System.DateTime(
-                new DateOnly(choice.Year.NumberValue,
-                    choice.Month.NumberValue,
-                    choice.Day.NumberValue),
-                new TimeOnly(choice.Hour.NumberValue,
-                    choice.Minute.NumberValue,
-                    choice.Second.NumberValue,
-                    choice.Millisecond.NumberValue,
-                    choice.Microsecond.NumberValue)));
+        Random random = new Random();
+        IRandomDateTime dateTimeGenerator = new RandomDateTimeGenerator();
 
-        Assert.Equal(valueOnTrue.Nanoseconds.NumberValue, choice.Nanoseconds.NumberValue);
+        IEnumerable<IDateTime> trueValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue().AddTicks(random.NextInt64(0, 1000)))
+            .Select(x => (date: DateOnly.FromDateTime(x), time: TimeOnly.FromDateTime(x)))
+            .Select(x => new DateTime(new Date(x.date), new Time(x.time)))
+            .ToArray();
+
+        IEnumerable<IDateTime> falseValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue().AddTicks(random.NextInt64(0, 1000)))
+            .Select(x => (date: DateOnly.FromDateTime(x), time: TimeOnly.FromDateTime(x)))
+            .Select(x => new DateTime(new Date(x.date), new Time(x.time)))
+            .ToArray();
+
+        IBool condition = new True();
+
+        IEnumerable<IDateTime> choices = trueValues.Zip(falseValues,
+            (trueValue, falseValue) => new DateTimeChoice(condition, trueValue, falseValue));
+
+        Assert.Equal(trueValues, choices, (choice, dateTime) =>
+            choice.Year.NumberValue == dateTime.Year.NumberValue &&
+            choice.Month.NumberValue == dateTime.Month.NumberValue &&
+            choice.Day.NumberValue == dateTime.Day.NumberValue &&
+            choice.Hour.NumberValue == dateTime.Hour.NumberValue &&
+            choice.Minute.NumberValue == dateTime.Minute.NumberValue &&
+            choice.Second.NumberValue == dateTime.Second.NumberValue &&
+            choice.Millisecond.NumberValue == dateTime.Millisecond.NumberValue &&
+            choice.Microsecond.NumberValue == dateTime.Microsecond.NumberValue &&
+            choice.Nanoseconds.NumberValue == dateTime.Nanoseconds.NumberValue);
     }
 
     [Fact]
     public void CorrectChooseOnFalseCondition()
     {
-        System.DateTime dateTimeOnTrue = new System.DateTime(2024, 11, 5, 1, 2, 3, 4, 5).AddTicks(1);
-        System.DateTime dateTimeOnFalse = new System.DateTime(2025, 12, 13, 6, 5, 4, 3, 2).AddTicks(2);
-        IDateTime valueOnTrue = new DateTime(new Date(DateOnly.FromDateTime(dateTimeOnTrue)), new Time(TimeOnly.FromDateTime(dateTimeOnTrue)));
-        IDateTime valueOnFalse = new DateTime(new Date(DateOnly.FromDateTime(dateTimeOnFalse)), new Time(TimeOnly.FromDateTime(dateTimeOnFalse)));
-        IDateTime choice = new DateTimeChoice(new False(), valueOnTrue, valueOnFalse);
-        Assert.Equal(
-            new System.DateTime(
-                new DateOnly(valueOnFalse.Year.NumberValue,
-                    valueOnFalse.Month.NumberValue,
-                    valueOnFalse.Day.NumberValue),
-                new TimeOnly(valueOnFalse.Hour.NumberValue,
-                    valueOnFalse.Minute.NumberValue,
-                    valueOnFalse.Second.NumberValue,
-                    valueOnFalse.Millisecond.NumberValue,
-                    valueOnFalse.Microsecond.NumberValue)),
-            new System.DateTime(
-                new DateOnly(choice.Year.NumberValue,
-                    choice.Month.NumberValue,
-                    choice.Day.NumberValue),
-                new TimeOnly(choice.Hour.NumberValue,
-                    choice.Minute.NumberValue,
-                    choice.Second.NumberValue,
-                    choice.Millisecond.NumberValue,
-                    choice.Microsecond.NumberValue)));
+        Random random = new Random();
+        IRandomDateTime dateTimeGenerator = new RandomDateTimeGenerator();
 
-        Assert.Equal(valueOnFalse.Nanoseconds.NumberValue, choice.Nanoseconds.NumberValue);
+        IEnumerable<IDateTime> trueValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue().AddTicks(random.NextInt64(0, 1000)))
+            .Select(x => (date: DateOnly.FromDateTime(x), time: TimeOnly.FromDateTime(x)))
+            .Select(x => new DateTime(new Date(x.date), new Time(x.time)))
+            .ToArray();
+
+        IEnumerable<IDateTime> falseValues = Enumerable.Range(0, 1000)
+            .Select(_ => dateTimeGenerator.GenerateValue().AddTicks(random.NextInt64(0, 1000)))
+            .Select(x => (date: DateOnly.FromDateTime(x), time: TimeOnly.FromDateTime(x)))
+            .Select(x => new DateTime(new Date(x.date), new Time(x.time)))
+            .ToArray();
+
+        IBool condition = new False();
+
+        IEnumerable<IDateTime> choices = trueValues.Zip(falseValues,
+            (trueValue, falseValue) => new DateTimeChoice(condition, trueValue, falseValue));
+
+        Assert.Equal(falseValues, choices, (choice, dateTime) =>
+            choice.Year.NumberValue == dateTime.Year.NumberValue &&
+            choice.Month.NumberValue == dateTime.Month.NumberValue &&
+            choice.Day.NumberValue == dateTime.Day.NumberValue &&
+            choice.Hour.NumberValue == dateTime.Hour.NumberValue &&
+            choice.Minute.NumberValue == dateTime.Minute.NumberValue &&
+            choice.Second.NumberValue == dateTime.Second.NumberValue &&
+            choice.Millisecond.NumberValue == dateTime.Millisecond.NumberValue &&
+            choice.Microsecond.NumberValue == dateTime.Microsecond.NumberValue &&
+            choice.Nanoseconds.NumberValue == dateTime.Nanoseconds.NumberValue);
     }
 
     [Fact]
